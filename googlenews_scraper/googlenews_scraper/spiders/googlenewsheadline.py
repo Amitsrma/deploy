08fb0be516fcb180f11d0d837a155d37ff7db5e0
Amit Sharma <45700624+Amitsrma@users.noreply.github.com>
2020-07-28 11:33:46 -0400

    Update googlenewsheadline.py

--- googlenews_scraper/googlenews_scraper/spiders/googlenewsheadline.py
+++ googlenews_scraper/googlenews_scraper/spiders/googlenewsheadline.py
@@ -4,8 +4,8 @@ import scrapy
 
 class GooglenewsheadlineSpider(scrapy.Spider):
     name = 'googlenewsheadline'
-    allowed_domains = ['https://newsapi.org/v2/everything?q=bitcoin&apiKey=0aa21d9d07ec4901b326c1507cbb5bb1']
-    start_urls = ['http://https://newsapi.org/v2/everything?q=bitcoin&apiKey=0aa21d9d07ec4901b326c1507cbb5bb1/']
+    allowed_domains = ['https://newsapi.org/v2/everything?q=bitcoin&apiKey=APIKEY']
+    start_urls = ['http://https://newsapi.org/v2/everything?q=bitcoin&apiKey=APIKEY/']
 
     def parse(self, response):
         pass
